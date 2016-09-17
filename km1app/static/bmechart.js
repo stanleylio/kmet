1cb68a5185fed93e3bcaa7103cdfce03c46227b7
Stanley Lio <stanleylio@gmail.com>
2016-09-17 10:35:48 +0000

    .

---
+++ km1app/static/bmechart.js
@@ -0,0 +1,212 @@
+$(function() {
+	var chart;
+
+	function addpoint(d) {
+		if (!(chart == null)) {
+			var window_size = 3600;
+			var ts = d['ts']*1000;
+			var t = d['T'];
+			var p = d['P'];
+			var rh = d['RH'];
+			var series = chart.series[0];
+			var shift = series.data.length > window_size;
+			chart.series[0].addPoint([ts,t],true,shift);
+			chart.series[1].addPoint([ts,p],true,shift);
+			chart.series[2].addPoint([ts,rh],true,shift);
+		}
+	}
+
+	function check_liveliness() {
+		function is_fresh() {
+			if (!(chart == null)) {
+				var max_t = 0;
+				for (var i = 0; i < chart.series[0].data.length; i++) {
+					var x = chart.series[0].data[i].x;
+					if (x > max_t) {
+						max_t = x;
+					}
+				}
+				return (Date.now() - max_t) < 2*60*1000;
+			}
+			//console.log("chart not ready");
+			return false;
+		}
+
+		if (is_fresh()) {
+			//console.log('fresh');
+			$('body').css("-webkit-filter","");
+			$('body').css("filter","");
+		} else {
+			//console.log('stale');
+			$('body').css("-webkit-filter","grayscale(100%)");
+			$('body').css("filter","grayscale(100%)");
+		}
+	}
+
+	//var url = "ws://localhost:9000/";
+	var url = "ws://" + String(window.location.host) + ":9000";
+	//ws = new WebSocket(url);
+	ws = new ReconnectingWebSocket(url);
+	ws.onopen = function(evt) {
+		console.log(evt)
+	};
+	ws.onclose = function(evt) {
+		console.log("closed")
+	};
+	ws.onmessage = function(evt) {
+		//console.log(evt.data);
+		var m = evt.data;
+		var i = m.indexOf(',');
+		if (m.substr(0,i).includes("_BME280")) {
+			var data = JSON.parse(m.substr(i+1));
+			addpoint(data);
+		}
+	};
+	ws.onerror = function(evt) {
+		console.log("error?")
+	};
+
+	var t_color = Highcharts.getOptions().colors[2];
+	var p_color = Highcharts.getOptions().colors[0];
+	var rh_color = Highcharts.getOptions().colors[3];
+
+	chart = new Highcharts.Chart({
+		chart: {
+			type: 'line',
+			renderTo: 'container',
+			defaultSeriesType: 'spline',
+			events: {
+				//load: addpoint
+			},
+			animation: false
+		},
+		title: {
+			text: 'Temperature, Barometric Pressure and Relative Humidity'
+		},
+		/*subtitle: {
+			text: 'Live data from a Bosch BME280 in the electrical box on the met. mast'
+		},*/
+		xAxis: {
+			type: 'datetime',
+			tickPixelInterval: 150,
+			minRange: 1000
+		},
+		yAxis: [{
+			minPadding: 0.2,
+			maxPadding: 0.2,
+			labels: {
+				format: '{value}°C',
+				style: {
+					color: t_color
+				}
+			},
+			title: {
+				text: 'Temperature',
+				margin: 30,
+				style: {
+					fontSize: '24px',
+					color: t_color
+				}
+			}
+		},{
+			gridLineWidth: 0,
+			labels: {
+				format: '{value}kPa',
+				style: {
+					color: p_color
+				}
+			},
+			title: {
+				text: 'Barometric Pressure',
+				style: {
+					fontSize: '24px',
+					color: p_color
+				}
+			},
+			opposite: true
+		},{
+			gridLineWidth: 0,
+			labels: {
+				format: '{value}%',
+				style: {
+					color: rh_color
+				}
+			},
+			title: {
+				text: 'Relative Humidity',
+				style: {
+					fontSize: '24px',
+					color: rh_color
+				}
+			},
+			opposite: true
+		}],
+		tooltip: {
+			shared: true
+		},
+		series: [{
+			name: 'Temperature',
+			data: [],
+			yAxis: 0,
+			color: t_color,
+			lineWidth: 3,
+			tooltip: {
+				valueSuffix: ' °C'
+			}
+		},{
+			name: 'Barometric Pressure',
+			data: [],
+			yAxis: 1,
+			color: p_color,
+			lineWidth: 3,
+			dashStyle: 'shortdash',
+			tooltip: {
+				valueSuffix: ' kPa'
+			}
+		},{
+			name: 'Relative Humidity',
+			data: [],
+			yAxis: 2,
+			color: rh_color,
+			lineWidth: 3,
+			dashStyle: 'shortdot',
+			tooltip: {
+				valueSuffix: ' %'
+			}
+		}],
+		tooltip: {
+			enabled: true
+		},
+		legend: {
+			enabled: true,
+			layout: 'vertical',
+			floating: true,
+			align: 'left',
+			verticalAlign: 'top',
+			y: 60,
+			x: 100,
+			backgroundColor: '#ffffff',
+		},
+		credits: {
+			enabled: false
+		},
+		/*plotOptions: {
+			area: {
+				fillColor: {
+					linearGradient: {
+						x1:0,
+						y1:0,
+						x2:0,
+						y2:1
+					},
+					stops: [
+						[0, Highcharts.getOptions().colors[2]],
+						[1, Highcharts.Color(Highcharts.getOptions().colors[2]).setOpacity(0).get('rgba')]
+					]
+				}
+			}
+		},*/
+	});		
+	
+	setInterval(check_liveliness,5*60*1000);
+});
